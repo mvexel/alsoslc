03ae5bb764e131966e5eb472a9bac1fc4b5cbe36
Martijn van Exel <m@rtijn.org>
2019-08-17 09:59:41 -0600

    finish deploy script for now, Good Enough

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo $ALSOSLC_SOURCEPATH
+# directories need to have trailing slashes!
+
 "${ALSOSLC_SOURCEPATH?ALSOSLC_SOURCEPATH not set}"
 "${ALSOSLC_SITEPATH?ALSOSLC_SITEPATH not set}"
+
 # generate site
 source venv/bin/activate
 ./publish.py $ALSOSLC_SOURCEPATH $ALSOSLC_SITEPATH
-rsync -avz $ALSOSLC_SOURCEPATH www-data@alsoslc.com:/var/www/html/alsoslc.com/public_html
+rsync -avz $ALSOSLC_SITEPATH www-data@alsoslc.com:/var/www/html/alsoslc.com/public_html
